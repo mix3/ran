664225bc81a2af463790d5526f1cdf3d17d45a01
mix3@ｻﾀﾃﾞｰﾅｲﾄﾌｨｰﾊﾞｰ <himachocost333@hotmail.co.jp>
2015-10-03 21:53:04 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/braintree/manners"
+	"github.com/lestrrat/go-server-starter/listener"
+)
+
+type Server struct {
+	*manners.GracefulServer
+}
+
+func New(svr *http.Server) *Server {
+	return &Server{manners.NewWithServer(svr)}
+}
+
+func Run(addr string, n http.Handler) {
+	srv := New(&http.Server{Addr: addr, Handler: n})
+
+	if err := srv.ListenAndServe(); err != nil {
+		if opErr, ok := err.(*net.OpError); !ok || (ok && opErr.Op != "accept") {
+			logger := log.New(os.Stdout, "[graceful] ", 0)
+			logger.Fatal(err)
+		}
+	}
+}
+
+func ListenAndServe(s *http.Server) error {
+	svr := New(s)
+	return svr.ListenAndServe()
+}
+
+func (srv *Server) newListener() (net.Listener, error) {
+	listeners, err := listener.ListenAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var l net.Listener
+	if len(listeners) == 0 {
+		addr := srv.Addr
+		if addr == "" {
+			addr = ":http"
+		}
+
+		l, err = net.Listen("tcp", addr)
+	} else {
+		l = listeners[0]
+	}
+
+	return l, err
+}
+
+func (srv *Server) ListenAndServe() error {
+	l, err := srv.newListener()
+	if err != nil {
+		return err
+	}
+
+	return srv.Serve(l)
+}
